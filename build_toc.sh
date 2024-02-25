4ff023d2e6ed94258492756d010258d34ee65a38
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-25 14:44:52 +0200

    Table of Contents script

--- build_toc.sh
+++ build_toc.sh
@@ -5,5 +5,5 @@ echo -e "# Table of Contents\n" >TOC.md
 IFS=$'\n'
 
 for FILEPATH in $(find . -path "*.md" | sed -e "s/^\.\///" -e "s/\.md$//"); do
-	echo "- ["$FILEPATH"](https://ganiulis.github.io/knowledge-base/"$(echo $FILEPATH | tr -s ' ' '%20')")" >>TOC.md
+	echo "- ["$FILEPATH"](https://ganiulis.github.io/knowledge-base/"$(echo $FILEPATH | sed -e 's/ /%20/g')")" >>TOC.md
 done
