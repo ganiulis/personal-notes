40a3273f628d9d237aee8cc002c222e832fbde0d
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-26 21:23:33 +0200

    Update TOC scripts

--- build_toc.sh
+++ build_toc.sh
@@ -5,5 +5,5 @@ echo -e "# Table of Contents\n" >TOC.md
 IFS=$'\n'
 
 for FILEPATH in $(find . -mindepth 2 -path "*.md" | sed -e "s/^\.\///" -e "s/\.md//"); do
-	echo "- ["$FILEPATH"](https://ganiulis.github.io/knowledge-base/"$(echo $FILEPATH | sed -e 's/ /%20/g')")" >>TOC.md
+	echo "- ["$FILEPATH"](https://ganiulis.github.io/personal-notes/"$(echo $FILEPATH | sed -e 's/ /%20/g')")" >>TOC.md
 done
