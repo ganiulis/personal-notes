b4498e5629dc92a2f6dbd3f1a3fbecc5c7b3808d
Gintaras Aniulis <gintarasa95@gmail.com>
2024-08-19 09:55:25 +0300

    refactor: Jekyll

--- build_toc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "# Table of Contents\n" >TOC.md
-
-IFS=$'\n'
-
-for FILEPATH in $(find . -mindepth 2 -path "*.md" | sed -e "s/^\.\///" -e "s/\.md//"); do
-	echo "- ["$FILEPATH"](https://ganiulis.github.io/personal-notes/"$(echo $FILEPATH | sed -e 's/ /%20/g')")" >>TOC.md
-done
